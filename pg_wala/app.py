dc5014134b2a8e245a4898e8d6b6d71cb73ab79d
AtharavaK <atharava0910@gmail.com>
2026-04-03 10:34:39 +0530

    Update app.py

--- pg_wala/app.py
+++ pg_wala/app.py
@@ -53,4 +53,5 @@ with app.app_context():
         print("✅ Default owner created → username: owner | password: admin123")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
